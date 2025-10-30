94535eb3e4aae72bcb1b29c6ef3d69000c719575
Lays Alves <santoslays67@gmail.com>
2025-10-30 17:35:08 -0300

    atualizaçaõ do index

---
+++ index.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata'; 
+import * as express from 'express';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
+// 2. Importar as configurações e rotas
+import { connectDatabase } from '../foco-na-rotina/src/config/database';
+import { studyRoutes } from '../foco-na-rotina/src/routes/studyRoutes';
+import { UserController } from './src/controller/usercontroller'; 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const userController = new UserController(); 
+
+app.use(cors());
+app.use(express.json());
+
+connectDatabase(); 
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('API de Estudos está online e funcionando!');
+});
+
+app.use('/api/studies', studyRoutes); 
+app.get('/api/users', userController.getAllUsers.bind(userController)); 
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Acesse: http://localhost:${PORT}`);
+});
